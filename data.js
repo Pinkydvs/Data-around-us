7cec408928f90b8ac8962d0f58e03724a5477671
Pinkydvs <116494570+Pinkydvs@users.noreply.github.com>
2022-11-02 16:04:42 +0200

    Update data.js
    Create a variable named year, and add your favorite book’s publication year as its value

--- data.js
+++ data.js
@@ -2,3 +2,5 @@ let title=['A Game of Thrones', 'The Hunger Games'];
 console.log(title);
 let author=['George R.R. Martin', 'Suzzane Collins'];
 console.log(author);
+let year=[1996, 2008];
+console.log(year);
